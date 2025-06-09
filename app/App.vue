0a29137d04074722b220ae7d780a2d74733c30fe
IJN Akashi AR <dabanya@protonmail.com>
2025-06-09 12:13:50 -0700

    tmp

--- app/App.vue
+++ app/App.vue
@@ -1,13 +1,54 @@
+<script setup>
+import { ref } from 'vue'
+
+const message = ref('Hello World!')
+
+const input = ref('input')
+
+function reverseMessage() {
+  // Access/mutate the value of a ref via
+  // its .value property.
+  message.value = message.value.split('').reverse().join('')
+}
+
+function notify() {
+  alert('navigation was prevented.')
+}
+</script>
+
 <template>
-  <div>Hello {{ name }}!</div>
+  <h1>LibreRiichi</h1>
+
+  <p>Join Room</p>
+  <p>Room Name <input v-bind:id="input"> <button>Find</button></p>
+
+  <!--
+    Note we don't need .value inside templates because
+    refs are automatically "unwrapped" in templates.
+  -->
+  <h1>{{ message }}</h1>
+
+  <!--
+    Bind to a method/function.
+    The @click syntax is short for v-on:click.
+  -->
+  <button @click="reverseMessage">Reverse Message</button>
+
+  <!-- Can also be an inline expression statement -->
+  <button @click="message += '!'">Append "!"</button>
+
+  <!--
+    Vue also provides modifiers for common tasks
+    such as e.preventDefault() and e.stopPropagation()
+  -->
+  <a href="https://vuejs.org" @click.prevent="notify">
+    A link with e.preventDefault()
+  </a>
 </template>
 
-<script>
-  export default {
-    data() {
-      return {
-        name: "Vue",
-      };
-    },
-  };
-</script>
+<style>
+button, a {
+  display: block;
+  margin-bottom: 1em;
+}
+</style>
