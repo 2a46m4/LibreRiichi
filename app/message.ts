ea435e87cdfa0dd0701c2e4274b9ccfa8987ef8e
IJN Akashi AR <gcciscool@protonmail.com>
2025-06-20 16:50:58 -0700

    2025-06-20

--- app/message.ts
+++ app/message.ts
@@ -1,80 +1,108 @@
 export enum MessageType {
-    // Messages that are sent from server to client
-    InitialMessageEvent,
-    JoinArenaEvent,
-    ServerArenaEvent,
-    ListArenasEvent,
-    CreateArenaEvent,
+	// Messages that are sent from server to client
+	InitialMessageEvent,
+	// JoinArenaEvent,
+	ServerArenaEvent,
+	// ListArenasEvent,
+	// CreateArenaEvent,
 
-// Messages that are sent from client to server
-    InitialMessageAction,
-    JoinArenaAction,
-    ServerArenaAction,
-    ListArenasAction,
-    CreateArenaAction,
+	// Messages that are sent from client to server
+	InitialMessageAction,
+	// JoinArenaAction,
+	// ServerArenaAction,
+	// ListArenasAction,
+	// CreateArenaAction,
 }
 
 export enum ArenaMessageType {
-    // Messages that are sent from game (server) to player (client)
-    PlayerJoinedEvent,
-    PlayerQuitEvent,
-    GameStartedEvent,
-    ArenaBoardEvent,
+	// Messages that are sent from game (server) to player (client)
+	PlayerJoinedEvent,
+	PlayerQuitEvent,
+	GameStartedEvent,
+	ArenaBoardEvent,
 
-    // Messages that are sent from player (client) to game (server)
-    StartGameAction,
-    PlayerAction,
-    PlayerQuitAction,
+	// Messages that are sent from player (client) to game (server)
+	StartGameAction,
+	PlayerAction,
+	PlayerQuitAction,
 
 }
 
-// export type Message = {
-//     message_type: MessageType
-//     data: any
-// }
+type MessageEntry<T extends MessageType = MessageType, D = any> = {
+	message_type: T
+	data: D
+}
 
-export type Message =  InitialMessageEvent | InitialMessageAction | ServerArenaEvent | ServerArenaAction | JoinArenaAction | JoinArenaEvent | ArenaMessage
+type MessageMap = {
+	[MessageType.InitialMessageEvent]: null
+	[MessageType.InitialMessageAction]: { name: '' }
+	[MessageType.ServerArenaEvent]: { arena_message: ArenaMessage }
+}
+
+type ConstrainedMap<M extends Record<MessageType, any>> = {
+	[K in keyof M & MessageType]: MessageEntry<K, M[K]>
+}
+
+type AllMessages = ConstrainedMap<MessageMap>[keyof MessageMap]
+
+export type Message = InitialMessageEvent
+	| InitialMessageAction
+	| ServerArenaEvent
+	| ServerArenaAction
+	| JoinArenaAction
+	| JoinArenaEvent
+	| CreateArenaEvent
+	| CreateArenaAction
 
 export type InitialMessageEvent = {
-    message_type: MessageType.InitialMessageEvent
-    data: null
+	message_type: MessageType.InitialMessageEvent
+	data: null
 }
 
 export type InitialMessageAction = {
-    message_type: MessageType.InitialMessageAction
-    data: { name: '' }
+	message_type: MessageType.InitialMessageAction
+	data: { name: string }
 }
 
 export type ServerArenaEvent = {
-    message_type: MessageType.ServerArenaEvent
-    data: { arena_message: ArenaMessage }
+	message_type: MessageType.ServerArenaEvent
+	data: { arena_message: ArenaMessage }
 }
 
 export type ServerArenaAction = {
-    message_type: MessageType.ServerArenaAction
-    data: {
-        arena_message: ArenaMessage
-    }
+	message_type: MessageType.ServerArenaAction
+	data: {
+		arena_message: ArenaMessage
+	}
 }
 
 export type JoinArenaAction = {
-    message_type: MessageType.JoinArenaAction
-    data: {
-        arena_name: string
-    }
+	message_type: MessageType.JoinArenaAction
+	data: {
+		arena_name: string
+	}
 }
 
 export type JoinArenaEvent = {
-    message_type: MessageType.JoinArenaEvent
-    data: {
-        success: boolean
-    }
+	message_type: MessageType.JoinArenaEvent
+	data: {
+		success: boolean
+	}
+}
+
+export type CreateArenaEvent = {
+	
+}
+
+export type CreateArenaAction = {
+
 }
 
 export type ArenaMessage = {
-    message_type: ArenaMessage
-    data: {
-        message_type: ArenaMessageType
-        data: any
-    }
+	message_type: ArenaMessage
+	data: {
+		message_type: ArenaMessageType
+		data: any
+	}
 }
+
