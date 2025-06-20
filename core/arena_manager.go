2f9dcdcd37dbb8adab709cc75913dc059f0638d1
IJN Akashi AR <dabanya@protonmail.com>
2025-06-20 02:59:23 -0700

    implement arena manaager

--- core/arena_manager.go
+++ core/arena_manager.go
@@ -1,18 +1,112 @@
 package core
 
-import "github.com/google/uuid"
+import (
+	"fmt"
+	"sync"
 
-// TODO: Temporary
+	"github.com/google/uuid"
+)
+
 type ArenaList struct {
-	arena Arena
+	arena map[uuid.UUID]*Arena
+	name  map[string]uuid.UUID
+
+	sync.RWMutex
 }
 
 var GlobalArenaList ArenaList = ArenaList{}
 
-func InitializeArenaList() {
-
+type ArenaNotFoundError struct {
+	arena_name string
 }
 
-func GetArena(uuid.UUID) *Arena {
-	return &GlobalArenaList.arena
+type SameNameError struct {
+	arena_name string
+}
+
+func (e ArenaNotFoundError) Error() string {
+	return fmt.Sprint("Arena {} is not found", e.arena_name)
+}
+
+func (e SameNameError) Error() string {
+	return fmt.Sprint("There is already an arena with the same name: {}", e.arena_name)
+}
+
+func GetArenaFromName(name string) (*Arena, error) {
+	GlobalArenaList.RLock()
+	defer GlobalArenaList.RUnlock()
+
+	uuid, ok := GlobalArenaList.name[name]
+	if !ok {
+		return nil, ArenaNotFoundError{name}
+	}
+
+	arena, ok := GlobalArenaList.arena[uuid]
+	if !ok {
+		return nil, ArenaNotFoundError{name}
+	}
+
+	return arena, nil
+}
+
+func GetArena(uuid uuid.UUID) (*Arena, error) {
+	GlobalArenaList.RLock()
+	defer GlobalArenaList.RUnlock()
+
+	arena, ok := GlobalArenaList.arena[uuid]
+	if !ok {
+		return nil, ArenaNotFoundError{uuid.String()}
+	}
+
+	return arena, nil
+}
+
+func RemoveArena(name string) error {
+	GlobalArenaList.Lock()
+	defer GlobalArenaList.Unlock()
+
+	uuid, ok := GlobalArenaList.name[name]
+	if !ok {
+		return ArenaNotFoundError{name}
+	}
+
+	delete(GlobalArenaList.name, name)
+	delete(GlobalArenaList.arena, uuid)
+	return nil
+}
+
+func RemoveArenaUUID(uuid uuid.UUID) {
+	GlobalArenaList.Lock()
+	defer GlobalArenaList.Unlock()
+	delete(GlobalArenaList.arena, uuid)
+}
+
+func GetArenaUUID(name string) (uuid.UUID, error) {
+	GlobalArenaList.RLock()
+	defer GlobalArenaList.RUnlock()
+
+	uuid, ok := GlobalArenaList.name[name]
+	if !ok {
+		return uuid, ArenaNotFoundError{name}
+	}
+
+	return uuid, nil
+}
+
+func CreateAndAddArena(name string) error {
+	GlobalArenaList.Lock()
+	defer GlobalArenaList.Unlock()
+
+	_, exists := GlobalArenaList.name[name]
+	if exists {
+		return SameNameError{name}
+	}
+
+	newUUID := uuid.New()
+	GlobalArenaList.name[name] = newUUID
+
+	newArena := CreateArena()
+	GlobalArenaList.arena[newUUID] = &newArena
+
+	return nil
 }
