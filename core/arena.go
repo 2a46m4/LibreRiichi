eed59681195ac834e17c98c26744756e18806065
IJN Akashi AR <gcciscool@protonmail.com>
2025-06-26 23:21:24 -0700

    Ye

--- core/arena.go
+++ core/arena.go
@@ -21,6 +21,7 @@ type Arena struct {
 	// AwaitingInputs []??? that stores the list of agents that it is waiting on
 
 	DateCreated time.Time
+	Name string
 
 	sync.Mutex
 }
@@ -32,6 +33,7 @@ type MessageSendInfo struct {
 }
 
 type ArenaInfo struct {
+	Name string
 	NumAgents   int
 	GameStarted bool
 	DateCreated time.Time
