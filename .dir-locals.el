6d3847c0fd9d243736099721c33f9489da63a25d
IJN Akashi AR <dabanya@protonmail.com>
2025-07-21 18:01:21 -0700

    tmp

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,4 +1,4 @@
 ;;; Directory Local Variables            -*- no-byte-compile: t -*-
 ;;; For more information see (info "(emacs) Directory Variables")
 
-((nil . ((compile-command . "yarn run watch"))))
+((nil . ((compile-command . "yarn run test"))))
--- cmd/eval_client/eval_client.go
+++ cmd/eval_client/eval_client.go
@@ -1,5 +1,27 @@
 package main
 
-func main() {
+import (
+	"github.com/gorilla/websocket"
+	"log"
+)
 
+func main() {
+	url := "ws://localhost:3000/game"
+	conn, resp, err := websocket.DefaultDialer.Dial(url, nil)
+	if err != nil {
+		log.Fatalf("Dial failed: %v (status: %v)", err, resp)
+	}
+	defer conn.Close()
+
+	err = conn.WriteMessage(websocket.TextMessage, []byte("Hello WebSocket!"))
+	if err != nil {
+		log.Println("Write error:", err)
+	}
+
+	_, msg, err := conn.ReadMessage()
+	if err != nil {
+		log.Println("Read error:", err)
+	} else {
+		log.Printf("Received: %s", msg)
+	}
 }
