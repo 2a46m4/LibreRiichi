9d978ba5822cb02ebc73ccd3ad6dc069c27b5d63
IJN Akashi AR <dabanya@protonmail.com>
2025-07-26 17:04:10 -0700

    action2

---
+++ core/game_data/action2.go
@@ -0,0 +1,58 @@
+package core
+
+type Action interface {
+	PerformAction()
+}
+
+type ActionWrapper struct {
+	Action
+}
+
+func (action ActionWrapper) MarshalJSON() ([]byte, error) {
+	switch action.Action.(type) {
+	case Ron:
+	}
+	return nil, nil
+}
+
+type Ron struct {
+	TileToRon Tile      `json:"tile_to_ron"`
+	WinResult WinResult `json:"win_result"` // TODO: Remove
+}
+
+func (ron Ron) PerformAction() {
+
+}
+
+type Tsumo struct {
+	TileToTsumo Tile `json:"tile_to_tsumo"`
+}
+
+type Riichi struct {
+	TileToRiichi Tile `json:"tile_to_riichi"`
+}
+
+type Toss struct {
+	TileToToss Tile `json:"tile_to_toss"`
+}
+
+type Skip struct {
+	ActionToSkip Action `json:"action_to_skip"`
+}
+
+type Pon struct {
+	TileToPon Tile `json:"tile_to_pon"`
+}
+
+type Kan struct {
+	TileToKan Tile `json:"tile_to_kan"`
+}
+
+type Chii struct {
+	TileToChii  Tile    `json:"tile_to_chii"`
+	TilesInHand [2]Tile `json:"tiles_in_hand"`
+}
+
+type Draw struct {
+	DrawnTile Tile `json:"drawn_tile"`
+}
