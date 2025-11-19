e6c164fd99c55ccbd6367fe49bada30ec7aa1107
IJN Akashi AR <gcciscool@protonmail.com>
2025-11-18 18:29:46 -0800

    add a quick check for combos

--- core/game/meld_finder/meld_finder.go
+++ core/game/meld_finder/meld_finder.go
@@ -25,6 +25,14 @@ type Meld struct {
 // Finds all combinations of winning Mahjong hands
 func FindMelds(tiles []Tile, alreadyCompleted int) (ret []WinningCombination) {
 
+	// Just need a pair
+	if alreadyCompleted == 4 && tiles[0] == tiles[1] {
+		ret = append(ret, WinningCombination{
+			Pair: tiles[0],
+		})
+		return
+	}
+
 	tileSet := NewMultiSet(tiles...)
 	pairs := findPairs(tileSet)
 	// Iterate over all possible combinations of pairs
@@ -40,9 +48,71 @@ func FindMelds(tiles []Tile, alreadyCompleted int) (ret []WinningCombination) {
 			copy(combo.Melds[:], meld)
 			ret = append(ret, combo)
 		}
+		tileSet.Add(pair, pair)
 	}
 
 	return ret
+}
+
+// Checks if a list of tiles has a winning combination. Since it's
+// only checking for one combination, it is potentially faster
+func HasWinningCombination(tiles []Tile, alreadyCompleted int) bool {
+	// Just need a pair
+	if alreadyCompleted == 4 && tiles[0] == tiles[1] {
+		return true
+	}
+
+	tileSet := NewMultiSet(tiles...)
+	pairs := findPairs(tileSet)
+	// Iterate over all possible combinations of pairs
+	for _, pair := range pairs {
+		tileSet.Remove(pair, pair)
+		if hasWinningCombination(tileSet, alreadyCompleted) {
+			return true
+		}
+		tileSet.Add(pair, pair)
+	}
+
+	return false
+}
+
+func hasWinningCombination(tileSet MultiSet[Tile], alreadyCompleted int) bool {
+	if alreadyCompleted == 4 {
+		return true
+	}
+
+	tileCopy := NewMultiSet(tileSet.ToSlice()...)
+	hasWinningCombo := func (tile1, tile2, tile3 Tile) bool {
+		tileCopy.Remove(tile1, tile2, tile3)
+		hasCombo := hasWinningCombination(tileCopy, alreadyCompleted + 1)
+		tileCopy.Add(tile1, tile2, tile3)
+		return hasCombo
+	}
+
+	for tile := range tileSet {
+		// Shuntsu
+		if tile.IsNumberTile() {
+			count1 := tileCopy.Count(tile)
+			count2 := tileCopy.Count(tile+1)
+			count3 := tileCopy.Count(tile+2)
+
+			if count1 >= 1 && count2 >= 1 && count3 >= 1 {
+				if hasWinningCombo(tile, tile+1, tile+2) {
+					return true
+				}
+			}
+		}
+
+		// Koutsu
+		count := tileCopy.Count(tile)
+		if count >= 3 {
+			if hasWinningCombo(tile, tile, tile) {
+				return true
+			}
+		}
+	}
+
+	return false
 }
 
 // Find all lists of melds in the set that are at least of length 4 - alreadyCompleted
