b057406900740b932efceb569a9a6620df377cf9
IJN Akashi AR <gcciscool@protonmail.com>
2025-11-28 21:25:57 -0800

    implement bezier interpolation

--- app/render/animation.ts
+++ app/render/animation.ts
@@ -1,5 +1,4 @@
 import * as THREE from 'three'
-import { TileObject } from './tile'
 
 // An object that exists in the scene and can be animated
 export interface IAnimatable {
@@ -61,6 +60,51 @@ export class TileLinearAnimation implements IAnimation {
 
     finished(): boolean {
         return this.is_finished
+    }
+}
+
+function fact(n: number) {
+    let v = 1
+    for (let i = 2; i <= n; i++) {
+	v *= i
+    }
+    return n
+}
+
+function binom(n: number, k: number) {
+    return fact(n) / (fact(k) * fact(n - k))
+}
+
+export class BezierAnimation implements IAnimation {
+    n: number
+    t: number = 0
+    is_finished: boolean = false
+    binom: number[] = []
+    
+    constructor(public points: THREE.Vector3[]) {
+	this.n = points.length - 1
+	for (let i = 0; i <= this.n; i++) {
+	    this.binom.push(binom(this.n, i))
+	}
+    }
+
+    next_step(dt: number): THREE.Vector3 {
+	let val = new THREE.Vector3()
+	for (let i = 0; i <= this.n; i++) {
+	    const v = Math.pow(1-this.t, this.n - i) * Math.pow(this.t, i)
+	    val.add(this.points[i].multiplyScalar(this.binom[i] * v))
+	}
+
+	if (this.t >= 1.0) {
+            this.is_finished = true
+            this.t = 1.0
+        }
+        this.t += dt
+	return val
+    }
+
+    finished(): boolean {
+	return this.is_finished
     }
 }
 
